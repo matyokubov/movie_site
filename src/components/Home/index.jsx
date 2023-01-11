478120e5a5201e511f9012708b0266fc3692f78e
user2020-py <x_bomba@inbox.ru>
2023-01-11 12:22:28 +0500

    msg_layout

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import Card from "../Card"
 import Carousel from "../Carousel"
 import { Container } from "./style"
+import MsgLayout from "../MsgLayout"
 
 import useRequest from "../../hooks/useRequest";
 import { useMemo, useReducer } from "react";
@@ -8,6 +9,7 @@ import { useMemo, useReducer } from "react";
 const Home = () => {
     console.log("Home");
     const [state, dispatch] = useReducer(reducer, {
+        failed: "wait", 
         popularPersons: [
                 {
                     title: "Unknown",
@@ -27,42 +29,64 @@ const Home = () => {
     function reducer(state, action){
         switch (action.type) {
             case "popular_persons":
-                return {...state, popularPersons: action.payload}
+                return {...state, popularPersons: action.payload, failed: false}
             case "popular_movies":
-                return {...state, popularMovies: action.payload}
+                return {...state, popularMovies: action.payload, failed: false}
+            case "fail":
+                return {...state, failed: true}
             default:
                 return state;
         }
     }
     useMemo(() => {
-        request({currUrl: "https://api.themoviedb.org/3", path: `/person/popular?api_key=b8297831d5876d2ce8a0fe891bf560d9`}).then(data => {dispatch({type: "popular_persons", payload: data.results}); console.log(data.results);})
-        request({currUrl: "https://api.themoviedb.org/3", path: `/movie/popular?api_key=b8297831d5876d2ce8a0fe891bf560d9`}).then(data => {dispatch({type: "popular_movies", payload: data.results}); console.log(data.results);})
+        request({
+            currUrl: "https://api.themoviedb.org/3",
+            path: `/person/popular?api_key=b8297831d5876d2ce8a0fe891bf560d9`
+        }).then(data => {
+            data.results ? dispatch({type: "popular_persons", payload: data.results}) : dispatch({type: "fail"})
+            console.log(data)
+        })
+        // ################################
+        request({
+            currUrl: "https://api.themoviedb.org/3",
+            path: `/movie/popular?api_key=b8297831d5876d2ce8a0fe891bf560d9`
+        }).then(data => {
+            data.results ? dispatch({type: "popular_movies", payload: data.results}) : dispatch({type: "fail"})
+            console.log(data);
+        })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+    console.log(state)
     return (
-        <>
-            <Carousel data={state.popularMovies?.slice(state.popularMovies?.length-8, state.popularMovies?.length)}/>
-            <Container className="container">
-                <h1>Popular Actors</h1>
-                <p style={{gridTemplateColumns: "repeat(7, 1fr)", gridTemplateRows: "repeat(2, 1fr)"}}>
-                    {
-                        state.popularPersons?.slice(0, 14).map((m, i) => (
-                            <Card type={"person"} title={m.name} description={m.overview} img={`https://image.tmdb.org/t/p/w185${m.profile_path}`} id={m.id} key={i}/>
-                        ))
-                    }
-                </p>
-            </Container>
-            <Container className="container">
-                <h1>Popular Movies</h1>
-                <p>
-                    {
-                        state.popularMovies?.map((m, i) => (
-                            <Card type={"movie"} title={m.original_title} description={m.description} img={`https://image.tmdb.org/t/p/w185${m.poster_path}`} id={m.id} key={i}/>
-                        ))
-                    }
-                </p>
-            </Container>
-        </>
+        state.failed=="wait" ? (
+            <MsgLayout text={"Please, wait..."}/>
+        ) : state.failed ? (
+            <MsgLayout text={"Connection Error"}/>
+        ) : (
+            <>
+                <Carousel data={state.popularMovies?.slice(state.popularMovies?.length-8, state.popularMovies?.length)}/>
+                <Container className="container">
+                    <h1>Popular Actors</h1>
+                    <p style={{gridTemplateColumns: "repeat(7, 1fr)", gridTemplateRows: "repeat(2, 1fr)"}}>
+                        {
+                            state.popularPersons?.slice(0, 14).map((m, i) => (
+                                <Card type={"person"} title={m.name} description={m.overview} img={`https://image.tmdb.org/t/p/w185${m.profile_path}`} id={m.id} key={i}/>
+                            ))
+                        }
+                    </p>
+                </Container>
+                <Container className="container">
+                    <h1>Popular Movies</h1>
+                    <p>
+                        {
+                            state.popularMovies?.map((m, i) => (
+                                <Card type={"movie"} title={m.original_title} description={m.description} img={`https://image.tmdb.org/t/p/w185${m.poster_path}`} id={m.id} key={i}/>
+                            ))
+                        }
+                    </p>
+                </Container>
+            </>
+        )
     )
 }
 
---
+++ src/components/MsgLayout/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+const MsgLayout = ({text}) => {
+    return (
+        <div className="box">
+            <h1>{text}</h1>
+        </div>
+    )
+}
+
+export default MsgLayout;
